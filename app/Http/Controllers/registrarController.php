885e233ad05bbdc546d45b1f9dc9b17adb15305c
AryDaniel <ary.lopez@cetis107.edu.mx>
2022-10-31 18:29:30 -0700

    registrarController-part1

---
+++ app/Http/Controllers/registrarController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+class registrarController extends Controller
+{
+    function registrar(){
+        return view('registrar');
+    }
+}
