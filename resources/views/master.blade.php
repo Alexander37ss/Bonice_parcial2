c08b764e30b72026d55c0c7e3d38d05ceb7aae9d
Alexander37ss <alexanderpalazuelos37@gmail.com>
2022-10-31 17:54:38 -0700

    Inicio Vista

---
+++ resources/views/master.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <script src="{{asset('js/jquery-3.6.1.js')}}"></script>
+    <title>Mi Escuela App</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="{{url('/')}}" ><img src="{{asset('/img/pinguino.png')}}" width="70px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                @yield('contenido')
+            </div>
+        </div>
+    </div>
+    <footer>
+        <hr>
+        <div align = "center">BonIce &copy; 2022</div>
+    
+    </footer>
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+    @yield('aaa')
+
+</body>
+</html>
